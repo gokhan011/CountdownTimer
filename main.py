a9827b9c305e8fb009951983d31f69048608a00e
gokhan <112945009+gokhan011@users.noreply.github.com>
2024-02-22 23:29:28 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#from countdown import countdown
+
+#countdown(mins=0,secs=5)
+
+
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = "{:02d} : {:02d}", format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -=1
+t = input("Enter the time in seconds: ")
+countdown(input(t))
+print("Time is up!")
+
